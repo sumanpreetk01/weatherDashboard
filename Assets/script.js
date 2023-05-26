757397984192fb2f7c9af4f985b7c3e95df58554
Sumanpreet Kaur <sharankaur@Sharans-MacBook-Air.local>
2023-05-26 10:48:08 -0400

    created HTML and JS shell

---
+++ Assets/script.js
@@ -0,0 +1,25 @@
+var cityDisplay = document.querySelector('#city-display')
+var searchFormEl = document.querySelector('search-form')
+var forecastDisplay = document.querySelector('#forecast-container')
+
+function getParams(){
+    
+    searchAPI
+}
+
+function printResults(){
+
+}
+
+function searchAPI(){
+
+    printResults
+}
+
+function handleSearchSubmit(event){
+    event.prevenDefault();
+
+    searchAPI
+}
+
+searchFormEl.addEventListener('submit',handleSearchSubmit)
