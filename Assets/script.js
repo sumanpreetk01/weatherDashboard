e8eae2e9272315098f0b5105f1c92306c99e49e4
Sumanpreet Kaur <sharankaur@sharans-air.lan>
2023-05-30 22:14:43 -0400

    added final code

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,4 @@
+
 function initPage(){
     const cityEL = document.getElementById('enter-city');
     const searchEL = document.getElementById('search-button');
@@ -11,8 +12,12 @@ function initPage(){
     var fiveDayEL = document.getElementById('fiveday-header');
     var todayWeatherEL = document.getElementById('today-weather')
     var searchHistory = JSON.parse(localStorage.getItem('search')) ||[];
+    var forecastContainer = document.getElementsByClassName('forecast-container')
 
     const APIKey = '48d210ed2b17bbe738f43d80471108c4'
+
+    dayjs.extend(window.dayjs_plugin_utc);
+    dayjs.extend(window.dayjs_plugin_timezone);
 
     function getWeather(cityName){
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
@@ -34,7 +39,7 @@ function initPage(){
             currentPicEL.setAttribute('src', 'https://openweathermap.org/img/wn/' + weatherPic + '@2x.png');
             currentPicEL.setAttribute('alt', data.weather[0].description)
 
-            currentTempEL.innerHTML = 'Temperature:' +k2f(data.main.temp) + '&#176F';
+            currentTempEL.innerHTML = 'Temperature:' +kToC(data.main.temp) + '&#176C';
             currentHumidityEL.innerHTML = "Humidity: " + data.main.humidity + "%";
             currentWindEL.innerHTML = "Wind Speed: " + data.wind.speed + " MPH";
 
@@ -54,6 +59,7 @@ function initPage(){
                 currentUVEL.append(UVIndex);
             });
 
+            //5-day Forecast
             let cityID = data.id;
             let forecastQueryURL = 'https://api.openweathermap.org/data/2.5/forecast?id=' + cityID + '&appid=' + APIKey;
 
@@ -62,13 +68,63 @@ function initPage(){
                 return response.json();
             })
             .then(function(data){
+                console.log(data.list)
+                var startDt = dayjs().add(1, "day").startOf("day").unix();
+                var endDt = dayjs().add(6, "day").startOf("day").unix();
+                let forecastArray = data.list
+                const forecastContainer = document.querySelector('.forecast-container');
+
+                forecastContainer.innerHTML = '';
+
+                for (let index = 0; index < forecastArray.length; index++) {
+                    if(forecastArray[index].dt >= startDt && forecastArray[index].dt < endDt){
+                        if(forecastArray[index].dt_txt.slice(11, 13) == "12"){
+                        const temp = forecastArray[index].main.temp
+                        const humidity = forecastArray[index].main.humidity
+                        console.log(temp)
+                        const date = forecastArray[index].dt_txt.slice(0,10)
+                        const windSpeed = forecastArray[index].wind.speed
+
+
+                        var card = document.createElement('div');
+                        card.classList.add('col-md-2', 'forecast', 'bg-primary', 'text-white', 'm-2', 'rounded');
+
+                        //Date
+                        var forecastDate = document.createElement('p');
+                        forecastDate.innerHTML = 'Date:' + date
+                        
+                        //Temperature
+                        var forecastTemp = document.createElement('p')
+                        forecastTemp.innerHTML = 'Temperature:' + kToC(temp) + '&#176C';
+
+                        //Humidity
+                        var forecastHumidity = document.createElement('p');
+                        forecastHumidity.innerHTML = 'Humidity:' + humidity;
+
+                        //Wind Speed
+                        var forecaseWindSpeed = document.createElement('p');
+                        forecaseWindSpeed.innerHTML = 'Wind Spped ' + windSpeed + 'MPH'
+
+                        card.appendChild(forecastDate);
+                        card.appendChild(forecastTemp);
+                        card.appendChild(forecastHumidity);
+                        card.appendChild(forecaseWindSpeed);
+
+                        forecastContainer.appendChild(card)
+
+                        }
+                        
+                    }
+                
+                }
+
                 fiveDayEL.classList.remove('d-none');
 
-            const forecast = document.querySelectorAll('.forecast');
-            for (let index = 0; index < forecast.length; index++) {
-                forecast[i].innerHTML = "";
+            // const forecast = document.querySelectorAll('.forecast');
+            // for (let index = 0; index < forecast.length; index++) {
+            //     forecast[i].innerHTML = "";
                 
-            }
+            // }
             
             })
         })
@@ -86,8 +142,8 @@ function initPage(){
         // renderSearchHistory();
     })
 
-    function k2f(K) {
-        return Math.floor((K - 273.15) * 1.8 + 32);
+    function kToC(K) {
+        return Math.floor((K - 273.15));
     }
 
     // function renderSearchHistory(){
@@ -111,6 +167,6 @@ let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=
 
      let forecastQueryURL = 'https://api.openweathermap.org/data/2.5/forecast?id=6167865&appid=48d210ed2b17bbe738f43d80471108c4'
      
-     fetch(UVQueryURL)
+     fetch(forecastQueryURL)
      .then(res => res.json())
       .then(data => console.log(data))
